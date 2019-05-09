99843ff37fc2660004b9827de7f00475346bb92a
Menna Ramadan <mennaa.ramadan@gmail.com>
2019-05-09 15:15:01 +0200

    working on navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import './App.css';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+
+import './App.css';
+import MovieForm from './components/movieForm';
+
 
 
 function App() {
   return (
-    <main className="container">
-      <Movies/>      
-    </main>
+    <React.Fragment>
+      <NavBar/>
+      <main className="container">
+        {/* <Movies/>       */}
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}/>
+          <Route path="/movies" component={Movies}/>
+          <Route path="/customers" component={Customers}/>
+          <Route path="/rentals" component={Rentals}/>
+          <Route path="/notfound" component={NotFound}/>
+          <Redirect from="/" exact to="/movies"/>
+          <Redirect to="/notFound"/>
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/customers.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Customers = () => {
+    return (  <h1>Cutomers</h1>);
+}
+ 
+export default Customers;
