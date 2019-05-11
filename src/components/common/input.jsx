6167e7b31e000c2532d495f7600bc6f93ccd6544
Menna Ramadan <mennaa.ramadan@gmail.com>
2019-05-11 17:16:13 +0200

    working on some enhancement and adding reusable component

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-const Input = ({name, label, value ,error,  onChange}) => {
+const Input = ({name, label, error, ...rest}) => {
     return (  <div className="form-group">
                 <label htmlFor={name}>{label}</label>
                 <input 
-                onChange={onChange} 
-                value={ value} 
+                {...rest} 
+                name={name}
                 id={name} 
-                name={name} type="text" className="form-control"/>
+                className="form-control"/>
              {error && <div className="alert alert-danger">{error}</div>}
             </div> 
             );
