d87863ff39c60985a2a5c2c47e47acca69edd436
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2019-05-14 13:17:49 +0200

    working on search

---
+++ src/components/common/searchBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SearchBox = () => {
+    return (
+        <input 
+        className="form-control mr-sm-2" 
+        type="search" placeholder="Search"/>
+    );
+}
+
+export default SearchBox;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import Pagination from './common/pagination';
 import Paginate from '../utils/paginate';
 import ListGroup from './common/listGroup';
 import MoviesTable from './moviesTable';
+import SearchBoxh from './common/searchBox';
 import _ from 'lodash';
 
 class Movies extends Component {
@@ -75,6 +76,7 @@ class Movies extends Component {
             </div>
             <div className="col">
                     <Link className="btn btn-primary mb-2" to="/movies/new">New Movie</Link>
+                    <SearchBoxh value={value} onChange={this.handleSearch} />
                     <p>Showing {totalCount} movies in the database</p>
                     <MoviesTable 
                         movies={movies}
